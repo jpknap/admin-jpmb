7aedda4473fdf116d7826507d951e470b2a85e87
JPMB <jp.knao@gmail.com>
2025-02-20 17:33:00 -0300

    test load file

--- public/index.php
+++ public/index.php
@@ -12,6 +12,10 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+echo "hola mudno";
+/**
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+ *
+ * */
